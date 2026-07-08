b8e1984643e85b6c2fac0dedde554d6f7bb67edb
smartronsolutions <support@smartronsolutions.co.uk>
2026-07-09 04:56:10 +0500

    1

--- s_odoo_saas_master/wizard/saas_odoo_instance_backup_restore_wizard.py
+++ s_odoo_saas_master/wizard/saas_odoo_instance_backup_restore_wizard.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models, _
+from odoo import fields, models, _
 from odoo.exceptions import ValidationError
 
 
@@ -8,16 +8,11 @@ class OdooInstanceBackupRestoreWizard(models.TransientModel):
 
     backup_id = fields.Many2one('saas.odoo.instance.backup', string='Backup', required=True, ondelete='cascade')
     odoo_version_id = fields.Many2one(related='backup_id.odoo_version_id', string='Backup Odoo Version')
+    restore_state = fields.Selection(related='backup_id.restore_state', string='Restore Status')
+    restore_status_message = fields.Char(related='backup_id.restore_status_message', string='Restore Status Message')
     instance_id = fields.Many2one('saas.odoo.instance', string='Restore To Instance', required=True,
         domain=[('state', '=', 'deploy'), ('is_template', '=', False)])
     confirmation = fields.Char(string="Type 'yes' to confirm")
-
-    @api.onchange('backup_id')
-    def _onchange_backup_id(self):
-        domain = [('state', '=', 'deploy'), ('is_template', '=', False)]
-        if self.backup_id.odoo_version_id:
-            domain.append(('odoo_version_id', '=', self.backup_id.odoo_version_id.id))
-        return {'domain': {'instance_id': domain}}
 
     def action_restore(self):
         self.ensure_one()
@@ -25,11 +20,9 @@ class OdooInstanceBackupRestoreWizard(models.TransientModel):
             raise ValidationError(_("Please enter 'yes' in the confirmation box before restoring the backup."))
         self.backup_id.action_restore_to(self.instance_id)
         return {
-            'type': 'ir.actions.client',
-            'tag': 'display_notification',
-            'params': {
-                'message': _("Restore started for %s. Reopen this backup to track the progress.") % self.instance_id.display_name,
-                'type': 'success',
-                'sticky': False,
-            }
+            'type': 'ir.actions.act_window',
+            'res_model': self._name,
+            'res_id': self.id,
+            'view_mode': 'form',
+            'target': 'new',
         }
